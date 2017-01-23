96948aa01e0056a70f6ee8638a83e8d9a551a09b
Adam Rainsby <adamrainsby@live.co.uk>
2017-01-23 01:14:15 +0000

    SVM solution

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -1,17 +1,22 @@
 #!/usr/bin/python
 
-""" 
+"""
     This is the code to accompany the Lesson 2 (SVM) mini-project.
 
-    Use a SVM to identify emails from the Enron corpus by their authors:    
+    Use a SVM to identify emails from the Enron corpus by their authors:
     Sara has label 0
     Chris has label 1
 """
-    
+
 import sys
 from time import time
 sys.path.append("../tools/")
 from email_preprocess import preprocess
+from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score
+import collections
+
+classifier = SVC(kernel='rbf', C=10000)
 
 
 ### features_train and features_test are the features for the training
@@ -19,12 +24,17 @@ from email_preprocess import preprocess
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
+# features_train = features_train[:len(features_train)/100]
+# labels_train = labels_train[:len(labels_train)/100]
 
+t0 = time()
+classifier.fit(features_train, labels_train)
+print "training time:", round(time()-t0, 3), "s"
 
+t1 = time()
+prediction = classifier.predict(features_test)
+print "predicting time:", round(time()-t1, 3), "s"
 
-#########################################################
-### your code goes here ###
+print (prediction == 1).sum()
 
-#########################################################
-
-
+print accuracy_score(labels_test, prediction)
